c0b411977bf44885d37a918a411c6ebdd849c1f5
Dang Dai Duong <127579534+gunzosi@users.noreply.github.com>
2024-08-24 13:37:41 +0700

    Change PATH REPOSITORY

---
+++ DSA/ROMAN2INTEGER/RomanToInteger_CS/RomanToIntegerV1.cs
@@ -0,0 +1,38 @@
+﻿namespace csharp;
+
+public class RomanToIntegerV1
+{
+    public static int RomanToInt(string s) {
+        var sum = 0;
+        var pre = 0;
+
+        for (var i = s.Length - 1; i >= 0; i--) {
+            var current = RomanToValue(s[i]);
+
+            if (current < pre) {
+                sum -= current;
+            } else {
+                sum += current;
+            }
+
+            pre = current;
+        }
+
+        return sum;
+    }
+
+    private static int RomanToValue(char ch)
+    {
+        return ch switch
+        {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'D' => 500,
+            'M' => 1000,
+            _ => 0
+        };
+    }
+}
---
+++ DSA/ROMAN2INTEGER/RomanToInteger_java/RomanToIntegerV2.java
@@ -0,0 +1,41 @@
+package RomanToInteger;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RomanToIntegerV2 {
+
+
+    public static void main(String[] args) {
+        System.out.println(romanToInt("MCMXCIV")); // 1994
+        System.out.println(romanToInt("IV")); // 4
+        System.out.println(romanToInt("III")); // 3
+        System.out.println(romanToInt("IV")); // 4
+        System.out.println(romanToInt("IX")); // 9
+        System.out.println(romanToInt("LVIII")); // 58
+    }
+
+    public static int romanToInt(String s) {
+        Map<Character, Integer> m = new HashMap<>();
+
+        m.put('I', 1);
+        m.put('V', 5);
+        m.put('X', 10);
+        m.put('L', 50);
+        m.put('C', 100);
+        m.put('D', 500);
+        m.put('M', 1000);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+                ans -= m.get(s.charAt(i));
+            } else {
+                ans += m.get(s.charAt(i));
+            }
+        }
+
+        return ans;
+    }
+}
